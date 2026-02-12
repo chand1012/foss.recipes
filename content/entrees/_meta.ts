db83c7a8d5a4041e02fb162ed59fc484f9be79d3
Chandler Lofland <chandler@chand1012.dev>
2026-02-12 02:52:40 +0000

    feat: add steak cheese and rice recipe

--- content/entrees/_meta.ts
+++ content/entrees/_meta.ts
@@ -2,4 +2,5 @@ export default {
   index: { title: "🍛 Entrées", display: "hidden" },
   corn_flaked_chicken: "Corn Flake Chicken",
   "gouda_mac_&_cheese": "Gouda Mac and Cheese",
+  steak_cheese_and_rice: "Steak Cheese and Rice",
 };
